b690aaffe1fa43f994ea27767addf2e8316feece
icebreakeris <43800401+icebreakeris@users.noreply.github.com>
2023-03-19 14:51:50 +0000

    remove dist files

--- resources/ts-template/dist/client.js
+++
@@ -1,1 +0,0 @@
-"use strict";(()=>{setInterval(()=>{let e=GetPlayerPed(-1),t=GetEntityCoords(e,!0),o={input:JSON.stringify(t)};console.log(o)},1e3);})();
--- resources/ts-template/dist/server.js
+++
@@ -1,1 +0,0 @@
-"use strict";(()=>{var t=()=>"this could be your test utility :)";var i={input:t()};console.log(i.input);})();

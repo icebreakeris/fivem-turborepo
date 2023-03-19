c712a2dec83169ec967d5827593fb85f5efd6494
icebreakeris <43800401+icebreakeris@users.noreply.github.com>
2023-03-19 14:49:36 +0000

    make turborepo discover the deeply nested ui project

---
+++ resources/ts-template/dist/client.js
@@ -0,0 +1,1 @@
+"use strict";(()=>{setInterval(()=>{let e=GetPlayerPed(-1),t=GetEntityCoords(e,!0),o={input:JSON.stringify(t)};console.log(o)},1e3);})();
---
+++ resources/ts-template/dist/server.js
@@ -0,0 +1,1 @@
+"use strict";(()=>{var t=()=>"this could be your test utility :)";var i={input:t()};console.log(i.input);})();
